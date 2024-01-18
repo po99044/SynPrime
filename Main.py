7f9fbd8c86e0ecc56003e009a4dbbaef57860874
Hyeong-Cheol Oh <70564803+po99044@users.noreply.github.com>
2024-01-18 14:58:37 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,60 +11,6 @@ import synony_dic
 import Pam_disrupt
 import Gen_Syn
 
-
-def buffer(RTPBS, totallen):
-    remn_buflen = totallen- len(RTPBS)
-
-    nucleo_lst = ["A","T","G","C"]
-    nucleo_dic = {"A":["A","G","C"], "T":["T","G","C"],"G":["T","A","G"],"C":["T","C","A"]}
-
-    inter_lst = []
-
-    if  remn_buflen > len(RTPBS):
-        y = (remn_buflen % len(RTPBS))
-        x = (remn_buflen // len(RTPBS))
-        for k in range(remn_buflen):
-            remn_buf = RTPBS * x + RTPBS[:y]        
-            sub_lst = nucleo_dic[remn_buf[k]]
-            a = sub_lst.copy()
-            s = random.choice(a)
-            if k<2:
-                inter_lst.append(s)
-            else:
-                if inter_lst[k-2] != inter_lst[k-1]:
-                    inter_lst.append(s)
-                elif s == inter_lst[k-1]:
-                    a.remove(s)
-                    s = random.choice(a)
-                    inter_lst.append(s)
-                else: 
-                    inter_lst.append(s)
-        buff = ''.join(inter_lst)
-        
-
-    else:
-        for k in range(remn_buflen):
-            ran_num = len(RTPBS) - remn_buflen + 1 
-            case = random.randint(1,ran_num)    
-            remn_buf = RTPBS[case-1:case-1+remn_buflen]
-            sub_lst = nucleo_dic[remn_buf[k]]
-            a = sub_lst.copy()
-            s = random.choice(a)
-            if k<2:
-                inter_lst.append(s)
-            else:
-                if inter_lst[k-2] != inter_lst[k-1]:
-                    inter_lst.append(s)
-                elif s == inter_lst[k-1]:
-                    a.remove(s)
-                    s = random.choice(a)
-                    inter_lst.append(s)
-                else: 
-                    inter_lst.append(s)
-        buff = ''.join(inter_lst)
-
-    return buff
-
 def complement(seq):
     nu_dic = {'A': 'T', 'G':'C', 'T': 'A', 'C': 'G'}
     seq_len = len(seq)
@@ -90,20 +36,20 @@ base_dic = {"A":["T","C","G"], "T":["A","C","G"], "C":["T","A","G"],"G":["A","C"
 nu_dic = {'A': 'T', 'G':'C', 'T': 'A', 'C': 'G'}
 
 ## Variable ##
-exo = "E0"
+exo = "E5"
 rem1 = ""  # CDS 시작부분 Codon 부족부분 있으면 표기
 rem2 = "" # CDS 끝부분 Codon 부족부분 있으면 표기
 
 OUT_DIR = exo
-os.mkdir("AR_Modified_HEK/%s" % OUT_DIR)
+os.mkdir("Output/%s" % OUT_DIR)
 
 # %%
 
-files = os.listdir("AR_DeepPRIME_HEK/AR_%s"  % exo) 
+files = os.listdir("Input/SNCA_%s"  % exo) 
 for file in files:
 
 
-    df = pd.read_parquet("AR_DeepPRIME_HEK/AR_%s/%s" % (exo, file))
+    df = pd.read_parquet("Input/SNCA_%s/%s" % (exo, file))
     if len(df) == 0: 
         print (file)
         pass
@@ -119,34 +65,28 @@ for file in files:
 
         # Input form1
 
+
         for i in df.index:
-            a = df.loc[i,"Edited74_On"]
-            pbs_len = df.loc[i,'PBSlen']
-            rtpbs_len = df.loc[i,"RT-PBSlen"]
+            pbs_len = df.loc[i,'PBS_len']
+            rtpbs_len = df.loc[i,"RT-PBS_len"]
             rt_len = rtpbs_len - pbs_len
             rha = df.loc[i,"RHA_len"]
             ID = df.loc[i,"ID"]
-            edit_pos = 60 + int(ID.split('_')[2][1:-1]) - 5  # 2 original, 3 modify
-            df.loc[i,"EDIT_POS"] = edit_pos
+            edit_pos = 60 + int(ID.split('_')[2][1:-1]) - 5
+            df.loc[i,"Edit_pos"] = edit_pos
 
-            # RTPBS
-            df.loc[i,"RTPBS_R"] = a.replace('x','')
-            df.loc[i,"RTPBS"] = reverse_complement(df.loc[i,"RTPBS_R"])
-            RTPBS = df.loc[i,"RTPBS"]
-
+            RTPBS = df.loc[i,"RT-PBS"]
+            df.loc[i,"RTPBS_R"] = reverse_complement(RTPBS)
 
             pbs_r = df.loc[i,"RTPBS_R"][:pbs_len]
-            pam_site = a.find(pbs_r) + pbs_len + 3  # PAM start site based on WT74
-            sgRNA = df.loc[i,'WT74_On'][pam_site - 19:pam_site] 
-
-
+            sgRNA = df.loc[i,'Spacer'][1:]
             df.loc[i,"sgRNA"] = sgRNA
-            df.loc[i,"LHA"] = int(df.loc[i,"RTlen"])- int(df.loc[i,"RHA_len"]) -1
+            df.loc[i,"LHA"] = int(df.loc[i,"RTT_len"])- int(df.loc[i,"RHA_len"]) -1
             
 
 
-            df.loc[i,"PBS"] = df.loc[i,"RTPBS"][rt_len:]
-            df.loc[i,"RTPBS"] = RTPBS[:rha] + RTPBS[rha].lower() + RTPBS[rha+1:]
+            df.loc[i,"PBS"] = df.loc[i,"RT-PBS"][rt_len:]
+            df.loc[i,"RT-PBS"] = RTPBS[:rha] + RTPBS[rha].lower() + RTPBS[rha+1:]
             
             # strand
             if sgRNA in ref:
@@ -167,14 +107,14 @@ for file in files:
             
             df.loc[i,"PAMdist_fromedit"] = abs(edit_pos-pam_loc)  # PAM start site. Edit pos으로부터상대적인. NGG에서 N 위치.
 
-        df = df.sort_values(by=["PE2max-e_score"], ascending= False)
-
-        df = df[['ID', 'WT74_On', 'Edited74_On', 'PBSlen', 'RTlen', 'RT-PBSlen',
-            'Edit_pos', 'RHA_len', 'PE2max-e_score', 'EDIT_POS', 'RTPBS_R', 'RTPBS',
-            'sgRNA', 'LHA', 'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit']]
-
+        df = df.sort_values(by=["PE2max_score"], ascending= False)
+        df.columns = ['ID', 'PE2max_score', 'Spacer', 'RTPBS', 'PBSlen', 'RTlen',
+            'RT-PBSlen', 'EDIT_POS', 'Edit_len', 'RHA_len', 'Target', 'RTPBS_R',
+            'sgRNA', 'LHA', 'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit']
 
         df = Pam_disrupt.pam_disrupt(df,alpha, ref2,ref,cds)
+
+
 
         if len(df.columns) != 25:
             
@@ -182,22 +122,23 @@ for file in files:
                 df = Gen_Syn.syn_gen(df, alpha, beta, cds, ref, ref2)
                 result = df
                 
-                result = result.sort_values(by="PE2max-e_score", ascending=False)
+                result = result.sort_values(by="PE2max_score", ascending=False)
                 try:
                     result = result.dropna(subset = ["SynonyRTPBS"], axis =0)
                     result = result.reset_index()
+                    result = result[result["RHA_len"] >= 1]
                     result["synony pam disrupt"] = "F"
                     result["Intended pam disrupt"] = "F"
                     result["Intend_to_pamdisrupt_dist"] = "F"
                     result["Pam disrupt codon"] = "F"
                     
 
-                    result =result[['ID', 'WT74_On', 'Edited74_On', 'PBSlen', 'RTlen', 'RT-PBSlen',
-            'Edit_pos', 'RHA_len', 'PE2max-e_score', 'EDIT_POS', 'RTPBS_R', 'RTPBS',
-            'sgRNA', 'LHA', 'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit',
-            'SynonyRTPBS', 'synony pam disrupt', 'Intended pam disrupt',
-            'Intend_to_pamdisrupt_dist', 'Pam disrupt codon', 'SynonyCodon', 'seq']]
-                    result.to_csv("AR_Modified_HEK/%s/%s.csv" % (OUT_DIR, file) )
+                    result =result[['ID', 'PE2max_score', 'Spacer', 'RTPBS', 'PBSlen', 'RTlen', 'RT-PBSlen',
+       'EDIT_POS', 'Edit_len', 'RHA_len', 'Target', 'RTPBS_R', 'sgRNA', 'LHA',
+       'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit', 'SynonyRTPBS',
+       'synony pam disrupt', 'Intended pam disrupt',
+       'Intend_to_pamdisrupt_dist', 'Pam disrupt codon', 'SynonyCodon', 'seq']]
+                    result.to_csv("Output/%s/%s.csv" % (OUT_DIR, file) )
                 except KeyError : print(file)         
             else:
                 
@@ -205,23 +146,24 @@ for file in files:
                 df = Gen_Syn.syn_gen(df, alpha, beta, cds, ref, ref2)
                 result = df
                 
-                result = result.sort_values(by="PE2max-e_score", ascending=False)
+                result = result.sort_values(by="PE2max_score", ascending=False)
 
                 try:
                     result = result.dropna(subset = ["SynonyRTPBS"], axis =0)
                     result = result.reset_index()
+                    result = result[result["RHA_len"] >= 1]
                     result["synony pam disrupt"] = "F"
                     result["Intended pam disrupt"] = "F"
                     result["Intend_to_pamdisrupt_dist"] = "F"
                     result["Pam disrupt codon"] = "F"
 
 
-                    result =result[['ID', 'WT74_On', 'Edited74_On', 'PBSlen', 'RTlen', 'RT-PBSlen',
-            'Edit_pos', 'RHA_len', 'PE2max-e_score', 'EDIT_POS', 'RTPBS_R', 'RTPBS',
-            'sgRNA', 'LHA', 'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit',
-            'SynonyRTPBS', 'synony pam disrupt', 'Intended pam disrupt',
-            'Intend_to_pamdisrupt_dist', 'Pam disrupt codon', 'SynonyCodon', 'seq']]
-                    result.to_csv("AR_Modified_HEK/%s/%s.csv" % (OUT_DIR, file) )
+                    result =result[['ID', 'PE2max_score', 'Spacer', 'RTPBS', 'PBSlen', 'RTlen', 'RT-PBSlen',
+       'EDIT_POS', 'Edit_len', 'RHA_len', 'Target', 'RTPBS_R', 'sgRNA', 'LHA',
+       'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit', 'SynonyRTPBS',
+       'synony pam disrupt', 'Intended pam disrupt',
+       'Intend_to_pamdisrupt_dist', 'Pam disrupt codon', 'SynonyCodon', 'seq']]
+                    result.to_csv("Output/%s/%s.csv" % (OUT_DIR, file) )
                 except KeyError : print(file)
 
         else:
@@ -238,24 +180,23 @@ for file in files:
 
             result = pd.concat([df1, df2])
 
-            result = result.sort_values(by="PE2max-e_score", ascending=False)
+            result = result.sort_values(by="PE2max_score", ascending=False)
             try:
 
                 result = result.dropna(subset = ["SynonyRTPBS"], axis =0)
 
                 result = result.reset_index()
+                result = result[result["RHA_len"] >= 1]
+                result =result[['ID', 'PE2max_score', 'Spacer', 'RTPBS', 'PBSlen', 'RTlen', 'RT-PBSlen',
+       'EDIT_POS', 'Edit_len', 'RHA_len', 'Target', 'RTPBS_R', 'sgRNA', 'LHA',
+       'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit', 'SynonyRTPBS',
+       'synony pam disrupt', 'Intended pam disrupt',
+       'Intend_to_pamdisrupt_dist', 'Pam disrupt codon', 'SynonyCodon', 'seq']]
 
-                result =result[['ID', 'WT74_On', 'Edited74_On', 'PBSlen', 'RTlen', 'RT-PBSlen',
-        'Edit_pos', 'RHA_len', 'PE2max-e_score', 'EDIT_POS', 'RTPBS_R', 'RTPBS',
-        'sgRNA', 'LHA', 'PBS', 'strand', 'PAM_LOC', 'PAMdist_fromedit',
-        'SynonyRTPBS', 'synony pam disrupt', 'Intended pam disrupt',
-        'Intend_to_pamdisrupt_dist', 'Pam disrupt codon', 'SynonyCodon', 'seq']]
 
 
-
-                result.to_csv("AR_Modified_HEK/%s/%s.csv" % (OUT_DIR, file) )
+                result.to_csv("Output/%s/%s.csv" % (OUT_DIR, file) )
             
             except KeyError:
                 print(file)
             
-        
